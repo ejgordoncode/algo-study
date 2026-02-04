d210f2bb3b2c8b85f4101bc8a0ecb886fc6e99bd
EJ Gordon <ejgordoncode@gmail.com>
2026-02-04 05:20:18 +0000

    Initialize Streamlit app with basic UI for algorithm visualization

---
+++ app.py
@@ -0,0 +1,9 @@
+
+# Imports
+import streamlit as st
+import polars as pl
+import numpy as np 
+
+# Streamlit UI Config
+st.title('AlgoVisualizer')
+st.write('Visualize sorting algorithms for studying, interview prep, and just for fun!')
